6252f2ff4e792ea0fc548b2104241b13e986a2d0
reeyapokhrel <reeyapokhrel.cs24@bmsce.ac.in>
2025-12-16 12:14:38 +0530

    Add files via upload

--- lab exam/Main.java
+++ lab exam/Main.java
@@ -1,81 +1,38 @@
-import java.util.Scanner;
-class Person {
-    String name;
-    int age;
 
-    Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+interface Vehicle {
+    void start();  
+
+    default void honk() {
+        System.out.println("Honking... Beep Beep!");
     }
 }
 
-class Employee extends Person {
-    int empid;
-    int pan;
-    double bas_sal;
-
-    Employee(int empid, String name, int age, int pan, double bas_sal) {
-        super(name, age); 
-        this.empid = empid;
-        this.pan = pan;
-        this.bas_sal = bas_sal;
+class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car started.");
     }
+}
 
-    double gross_sal() {
-        double DA = 0.4 * bas_sal;
-        double HRA = 0.2 * bas_sal;
-        double CCA = 250;
-        return bas_sal + DA + HRA + CCA;
+class Bike implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Bike started.");
     }
-
-    double deduct() {
-        double PF = 0.1 * bas_sal;
-        double PT = 100;
-        double IT = 0.1 * gross_sal();
-        return PF + PT + IT;
-    }
-
-    double net_income() {
-        return gross_sal() - deduct();
+    @Override
+    public void honk() {
+        System.out.println("Bike honking... Peep Peep!");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Employee[] employees = new Employee[5];
+        Vehicle car = new Car();
+        car.start();
+        car.honk();  
 
-        System.out.println("Enter employee details:");
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Employee " + (i + 1) + " ID:");
-            int empid = sc.nextInt();
-
-            System.out.println("Enter employee name:");
-            String name = sc.next();
-
-            System.out.println("Enter employee age:");
-            int age = sc.nextInt();
-
-            System.out.println("Enter employee basic salary:");
-            double bas_sal = sc.nextDouble();
-
-            System.out.println("Enter employee PAN:");
-            int pan = sc.nextInt();
-
-            employees[i] = new Employee(empid, name, age, pan, bas_sal);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            Employee e = employees[i];
-            System.out.println("\nDetails for Employee " + (i + 1) + ":");
-            System.out.println("Name: " + e.name);
-            System.out.println("Age: " + e.age);
-            System.out.println("Gross Salary: " + e.gross_sal());
-            System.out.println("Deductions: " + e.deduct());
-            System.out.println("Net Salary: " + e.net_income());
-        }
-
-        sc.close();
+        Vehicle bike = new Bike();
+        bike.start();
+        bike.honk(); 
     }
 }
